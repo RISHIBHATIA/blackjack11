75cd81bab11478d992e5f93b4d4e7238585a23a3
RISHIBHATIA <RISHIBHATIA81@GMAIL.COM>
2018-05-30 21:23:48 -0400

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,48 @@
+console.log("hello ");
+
+function player1Hand() {
+var count1=Math.floor(Math.random()*10+1);
+var count2=Math.floor(Math.random()*10+1);
+var total1=count1+count2
+console.log("Player one has" + total1);
+}
+
+
+
+function player2Hand() {
+var count1=Math.floor(Math.random()*10+1);
+var count2=Math.floor(Math.random()*10+1);
+var total2=count1+count2
+console.log("Player two has" + total2);
+}
+
+function dealerHand() {
+var count1=Math.floor(Math.random()*10+1);
+var count2=Math.floor(Math.random()*10+1);
+var totald=count1+count2
+console.log("Dealer has" + totald);
+}
+
+
+player1Hand()
+player2Hand()
+dealerHand()
+
+if (player1Hand() < dealerHand()){
+console.log("Player One has" + player1Hand() +"is Beating the dealer" + dealerHand);}
+else {console.log("r");
+}
+
+
+
+// make a method in a function 
+
+
+//if player1Hand() == dealerHand() 
+//console.log("it's a push");
+//else 
+//console.log ("on the right tack ");
+
+//var hit =prompt("Do you want to hit ?");
+ //var alpha = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+ 
